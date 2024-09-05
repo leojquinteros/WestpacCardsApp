54252f0d6b3560a021255c682033f6705550f817
Leo Quinteros <leojquinteros@gmail.com>
2024-09-05 20:58:46 +1200

    First commit

---
+++ WestpacCardsApp/WestpacCardsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WestpacCardsAppApp.swift
+//  WestpacCardsApp
+//
+//  Created by Leo Quinteros on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WestpacCardsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
